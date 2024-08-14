f3d88cdd59be5b410649de2416d8eb913e97a856
Ego <augurto.17@gmail.com>
2024-08-14 17:22:32 -0500

    mejora de diseño

--- registrar_habitacion/index.php
+++ registrar_habitacion/index.php
@@ -6,15 +6,15 @@ include('../parts/nav.php');
 
 <div class="container mt-5">
         <h2>Crear Habitación</h2>
-        <form>
-            <div class="form-group">
+        <form action="procesar_habitacion.php" method="post" enctype="multipart/form-data">
+            <div class="form-group mb-3">
                 <label for="roomNumber">Número de Habitación</label>
-                <input type="text" class="form-control" id="roomNumber" placeholder="Ingrese el número de habitación" required>
+                <input type="text" class="form-control" id="roomNumber" name="roomNumber" placeholder="Ingrese el número de habitación" required>
             </div>
             
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="floor">Piso</label>
-                <select class="form-control" id="floor" required>
+                <select class="form-control" id="floor" name="floor" required>
                     <option value="" disabled selected>Seleccione el piso</option>
                     <option value="1">Piso 1</option>
                     <option value="2">Piso 2</option>
@@ -25,9 +25,9 @@ include('../parts/nav.php');
                 </select>
             </div>
 
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="bedType">Tipo de Cama</label>
-                <select class="form-control" id="bedType" required>
+                <select class="form-control" id="bedType" name="bedType" required>
                     <option value="" disabled selected>Seleccione el tipo de cama</option>
                     <option value="simple">Cama Simple</option>
                     <option value="doble">Cama Doble</option>
@@ -35,33 +35,28 @@ include('../parts/nav.php');
                 </select>
             </div>
 
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="bathType">Tipo de Baño</label>
-                <select class="form-control" id="bathType" required>
+                <select class="form-control" id="bathType" name="bathType" required>
                     <option value="" disabled selected>Seleccione el tipo de baño</option>
                     <option value="compartido">Baño Compartido</option>
                     <option value="incluido">Baño Incluido</option>
                 </select>
             </div>
 
-            <div class="form-group">
+            <div class="form-group mb-3">
                 <label for="price">Precio</label>
-                <input type="number" class="form-control" id="price" placeholder="Ingrese el precio" required>
+                <input type="number" class="form-control" id="price" name="price" placeholder="Ingrese el precio" required>
             </div>
 
-            <div class="form-group">
-                <label for="imageUpload">Subir Imagen</label>
-                <input type="file" class="form-control-file" id="imageUpload" required>
+            <div class="mb-3">
+                <label for="formFile" class="form-label">Subir Imagen</label>
+                <input class="form-control" type="file" id="formFile" name="image" required>
             </div>
 
             <button type="submit" class="btn btn-primary">Guardar Habitación</button>
         </form>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
 <?php
 include ('../parts/body_end.php');
 ?>
