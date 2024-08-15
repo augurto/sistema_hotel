136b9e18bb7708682111e4e90377e3b5cf61edc3
Ego <augurto.17@gmail.com>
2024-08-14 19:48:32 -0500

    quitando head

--- ver_habitaciones/index.php
+++ ver_habitaciones/index.php
@@ -21,15 +21,7 @@ include('../parts/datatable.php');
 include('../parts/nav.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ver Habitaciones</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+
     <div class="container mt-5">
         <h2>Listado de Habitaciones</h2>
         
